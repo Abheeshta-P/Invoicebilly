03c95352d2bf2d276bd7851b311b5fb998a2651b
Abheeshta-P <softwarecheck01+github@gmail.com>
2025-11-01 12:34:31 +0530

    intializespring

---
+++ backend/src/main/java/com/Invoicebilly/InvoicebillyApplication.java
@@ -0,0 +1,13 @@
+package com.Invoicebilly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicebillyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicebillyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/Invoicebilly/InvoicebillyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Invoicebilly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicebillyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
