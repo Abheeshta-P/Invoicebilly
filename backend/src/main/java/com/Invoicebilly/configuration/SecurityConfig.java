50bc598cb5fdb38f0112cb0efefcdd3bd4e67ee9
Abheeshta-P <softwarecheck01+github@gmail.com>
2025-12-01 18:38:52 +0530

    add render deploy docker file

--- backend/src/main/java/com/Invoicebilly/configuration/SecurityConfig.java
+++ backend/src/main/java/com/Invoicebilly/configuration/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
 
     private CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://invoicebilly.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
         configuration.setAllowCredentials(true);
